1d42b364ceb5ced5e900f328aaee36589ab2e7e5
Baranenkov Gleb <gbaranenkov@gmail.com>
2022-07-06 21:31:28 +0300

    Test1: первый коммит

---
+++ src/mypackage/TestClass.java
@@ -0,0 +1,9 @@
+package mypackage;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.print("Hello? ");
+        System.out.println("It's my first programm.");
+    }
+}
